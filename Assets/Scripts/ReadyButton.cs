da685d81576623c356de95025a4129aaf0d2f825
Vico <Victor@r-s.nu>
2026-05-03 13:52:40 +0200

    ui

--- Assets/Scripts/ReadyButton.cs
+++ Assets/Scripts/ReadyButton.cs
@@ -1,10 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ReadyButton : MonoBehaviour
 {
+    public GameObject EscapeMenu;
+
+    public GameObject _Help;
     public void Ready(GameObject button)
     {
         button.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame &&!_Help.activeSelf )
+        {
+            escape();
+            
+        }
+
+     
+        
+    }
+
+    public void activeSelf(GameObject self)
+    {
+        self.SetActive(!self.activeSelf);
+    }
+
+    public void escape()
+    {
+            EscapeMenu.SetActive(!EscapeMenu.activeSelf );
+            
+            if (Time.timeScale == 1f)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
     }
 
 }
